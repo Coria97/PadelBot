1e03193baa6c5ef9ac1aafeb1a7a4af2f31d4d39
Coria97 <scoria@stormtech.com.ar>
2025-06-15 16:22:52 -0300

    rename file and class about telegram bot

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .config import BASE_URL, CHECK_INTERVAL, ENABLE_NOTIFICATIONS
 from .logger import setup_logger
 from .scraper import PadelScraper
-from .telegram_notifier import TelegramNotifier
+from .telegram_bot import TelegramBot
